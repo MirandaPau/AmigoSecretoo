ac3bc885fbe0c98d4cd8faa385a4dd3af73e494f
paumiranda <paulabelumiranda@gmial.com>
2025-03-02 13:09:10 -0300

    Explicacion de la parte logica

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];
+let amigos = []; // creo el array
 
 function agregarAmigo() {
-    let input = document.getElementById("amigo");
-    let nombre = input.value.trim();
+    let input = document.getElementById("amigo");//obtengo el nombre ingresado por el usuario
+    let nombre = input.value.trim();//elimina cualqier espacio en blnaco que s epueda colocra por accidente
     
-    if (nombre === "") {
+    if (nombre === "") {// si no hay ningun nombre cargado
         alert("No ha cargado ningun nombre, por favor ingrese un nombre ");
-        return;
+        return;// devuelve 
     }
     
-    amigos.push(nombre);
-    input.value = "";
-    mostrarLista();
+    amigos.push(nombre);// agrego el nombre
+    input.value = "";//limpio el campo
+    mostrarLista();//llamo a la funcion 
 }
 
 function mostrarLista() {
-    let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
+    let lista = document.getElementById("listaAmigos");//obtine el id 'listaAmigos'
+    lista.innerHTML = "";//limpiamos el contenido antes de mostrar 
     
-    for (let i = 0; i < amigos.length; i++) {
-        let li = document.createElement("li");
-        li.textContent = amigos[i];  
-        lista.appendChild(li);
+    for (let i = 0; i < amigos.length; i++) {// recorremos el array
+        let li = document.createElement("li");// se crea un nuev elemento de lista
+        li.textContent = amigos[i];  //asigna el nombre actual del array al contenido del elemento <li>. 
+        lista.appendChild(li);//agrega un elemento
     }
 }
 
 function sortearAmigo() {
-    if (amigos.length === 0) {
+    if (amigos.length === 0) {//sino hay nombres en el array
         alert("Agrega nombres antes de sortear");
-        return;
+        return;//devuelve
     }
     
-    let indice = Math.floor(Math.random() * amigos.length);
-    let resultado = document.getElementById("resultado");
-    resultado.innerHTML =  `<li>El amigo secreto es: <strong>${amigos[indice]}</strong></li>`;
+    let indice = Math.floor(Math.random() * amigos.length);//genera un nombre aletorio
+    let resultado = document.getElementById("resultado");//obtengo el elemento
+    resultado.innerHTML =  `<li>El amigo secreto es: <strong>${amigos[indice]}</strong></li>`;//muestra el nombre del amigo sorteado
 }
